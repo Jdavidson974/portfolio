f5b83d70a036f190c7c8d13b2636fea9118dc0ec
JD <jd9743101@gmail.com>
2023-08-15 17:29:04 +0200

    init project structure + init navbar

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './components/layout/layout.component';
+import { SharedModule } from '../shared/shared.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule,
+    SharedModule
+  ]
+})
+export class LayoutModule { }
