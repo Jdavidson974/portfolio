99ac3b01415199adba77a420ac921bcb2522f092
JD <jd9743101@gmail.com>
2023-10-03 13:38:41 +0200

    ajout de quelque exp en dur

--- src/app/home/components/home-page/home-page.component.ts
+++ src/app/home/components/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Experience } from '../../models/experience.model';
 
 @Component({
   selector: 'app-home-page',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  expArray: Experience[] = [
+    {
+      img: "assets/img/logo.png",
+      content: "Projet Stanlinks , ce projet est fait avec Angular et NestJs pour l'API.\
+      Ce projet est une plateforme de partage de liens destinée aux utilisateurs des réseaux sociaux, accessible via une page personnalisable.",
+      link: "https://stlk.fr"
+    },
+    {
+      img: "assets/img/hope.jpg",
+      content: "Projet réservation sur site hope-location, sur ce site Wordpress j'ai dû ajouter un système de réservation en ligne avec paiement en ligne.\
+      Le paiement en ligne est géré grâce à Stripe, tandis que les réservations sont effectuées à l'aide de shortcodes qui communiquent avec l'API d'un prestataire appelé MyRentalCar.",
+      link: "https://www.hope-location.re",
+    },
+    {
+      img: "assets/img/manzer.jpg",
+      content: "Projet Manzer , ce projet est un projet fictif réalisé en formation, qui permet de la gestion de repas en ligne avec un compte Google.\
+      Ce projet est fait avec Angular est NestJs pour L'API ",
+      link: "https://ecf.sautronjeandavidson.re"
+    },
+    {
+      img: "assets/img/logo.png",
+      content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, aliquam quidem autem veritatis,\
+            architecto voluptatibus sit temporibus dolorum praesentium necessitatibus blanditiis doloremque ea iure\
+            libero rerum dolores explicabo quas optio.",
+    },
+
+  ]
 }
---
+++ src/app/home/models/experience.model.ts
@@ -0,0 +1,6 @@
+export class Experience {
+    img!: string;
+    content!: string;
+    link?: string;
+    git?: string;
+}
